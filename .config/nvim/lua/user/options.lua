934989793586294747c2f6d34ab81050e68812af
arun pandey <pandeyarun279@gmail.com>
2022-11-27 12:01:50 +0100

    tmux set ressurect dir

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -33,7 +33,7 @@ vim.opt.signcolumn = "yes" -- always show the sign column, otherwise it would sh
 vim.opt.wrap = true -- display lines as one long line
 vim.opt.scrolloff = 999 -- is one of my fav
 vim.opt.sidescrolloff = 8
-vim.opt.guifont = "monospace:h11" -- the font used in graphical neovim applications
+vim.opt.guifont = "SauceCodePro Nerd Font Mono:h11" -- the font used in graphical neovim applications
 vim.opt.wildignore =
 	"*.pyc,*.o,*.obj,*.svn,*.swp,*.class,*.hg,*.DS_Store,*.min.*,*.tmp,*.toc,*.aux,*.log,*.bbl,*.blg,*.log,*.lof,*.lot,*.ilg,*.out,*.glo,*.gls,*.nlo,*.nls,*.brf,*.ist,*.glg,*.synctex.gz,*.tgz,*.idx,*.ind,*blx.bib,*.fdb_latexmk,*.run.xml,*.bcf,*.glsdefs,*.fls,*.eps,*.pdf,*.png,*.jpg" -- Show only files editable by vim
 vim.opt.shortmess:append("c")
