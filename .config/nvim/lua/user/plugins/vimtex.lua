01da0e3cf93aff77e3e68f821b5baa87ef4c84c7
arun pandey <pandeyarun279@gmail.com>
2023-03-15 09:39:22 +0100

    for nomenclature,glossaries

--- .config/nvim/lua/user/plugins/vimtex.lua
+++ .config/nvim/lua/user/plugins/vimtex.lua
@@ -1,23 +1,4 @@
---[[ local status_ok, vimtex = pcall(require, "vimtex") ]]
---[[ if not status_ok then ]]
---[[ 	vim.notify(vimtex, vim.log.levels.ERROR) ]]
---[[   return ]]
---[[ end ]]
-
--- Vimtex options ---------------------------------
---[[ vim.g.vimtex_syntax_conceal = { ]]
---[[   accents = 1, ]]
---[[   cites = 1, ]]
---[[   fancy = 1, ]]
---[[   greek = 1, ]]
---[[   math_bounds = 1, ]]
---[[   math_delimiters = 1, ]]
---[[   math_fracs = 1, ]]
---[[   math_super_sub = 1, ]]
---[[   math_symbols = 1, ]]
---[[   sections = 1, ]]
---[[   styles = 1, ]]
---[[ } ]]
+-- requires ~/.latexmkrc for nomenclature, glossary generation
 
 vim.g.vimtex_quickfix_mode = 0
 vim.g.vimtex_quickfix_open_on_warning = 0
